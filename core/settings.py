ed1e8dc48df45156229a5a4b3644ced4fae69aa2
MdNazmul9 <nazmul.cse48@gmail.com>
2021-12-20 09:28:52 +0600

    postgres database setup complete

--- core/settings.py
+++ core/settings.py
@@ -64,12 +64,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql', #1
+        'NAME': 'newDb',
+        'USER': 'nazmul',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT': '5432',
+       }
 }
+
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
